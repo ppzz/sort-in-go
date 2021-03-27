9a958961b0bf707a9463604c7b4538d25674cce5
ppzz <penn.chn@gmail.com>
2021-03-28 04:14:08 +0800

    config(config): new config file

--- cmd/check-file/check-file.go
+++ cmd/check-file/check-file.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	Checker "github.com/ppzz/sorting-go/internal/checker"
+	"github.com/ppzz/sorting-go/internal/util"
+	"github.com/spf13/viper"
 	"path"
 	"strconv"
 )
 
 func main() {
-	assetRoot := "asset/"
-	filename := "int-" + strconv.Itoa(100) + ".txt"
-	absPath := path.Join(assetRoot, filename)
+	viper.AddConfigPath("./config")
+	viper.SetConfigName("dev") // 读取yaml配置文件
+	err := viper.ReadInConfig()
+	util.NoError(err)
+
+	NumCount := viper.GetInt("randomFile.numCount")
+	AssetRoot := viper.GetString("assetRoot")
+
+	filename := "int-" + strconv.Itoa(NumCount) + ".txt"
+	absPath := path.Join(AssetRoot, filename)
 	fc := Checker.NewFileChecker(absPath)
 	result, count := fc.Check()
 	fmt.Printf("file: %s, odered stats: %v, sorted numbers: %d\n", absPath, result, count)
--- cmd/gen-file/gen-file.go
+++ cmd/gen-file/gen-file.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ppzz/sorting-go/internal/rand-gen"
+	"github.com/ppzz/sorting-go/internal/util"
+	"github.com/spf13/viper"
 	"path"
 	"strconv"
+	"time"
 )
 
 func main() {
-	assetRoot := "asset/"
-	count := 100
-	filename := "int-" + strconv.Itoa(100) + ".txt"
 
-	absPath := path.Join(assetRoot, filename)
+	viper.AddConfigPath("./config")
+	viper.SetConfigName("dev") // 读取yaml配置文件
+	err := viper.ReadInConfig()
+	util.NoError(err)
+
+	NumCount := viper.GetInt("randomFile.numCount")
+	AssetRoot := viper.GetString("assetRoot")
+
+	filename := "int-" + strconv.Itoa(NumCount) + ".txt"
+	absPath := path.Join(AssetRoot, filename)
 	fg := RandGen.NewFileGen(absPath)
-	fg.GenVisible(count)
+	start := time.Now().UnixNano()
+	fg.GenVisible(NumCount)
+	end := time.Now().UnixNano()
+	fmt.Printf("use time: %d ms", (end-start)/1000/1000)
 }
